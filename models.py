b04766352dc7be27078e1fcecf0610e905406710
RickMohamedFiri <rick.mohamed@student.moringaschool.com>
2023-10-06 07:05:25 +0300

    added authentication features with jwt

--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from uuid import uuid4
+# from uuid import uuid4
 
 db = SQLAlchemy()
 
-def get_uuid():
-    return uuid4().hex
+# def get_uuid():
+#     return uuid4().hex
 
 # User Model
 class User(db.Model):
-    id = db.Column(db.String(11), primary_key=True, default=get_uuid)
+    id = db.Column(Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(60), nullable=False)
@@ -20,7 +20,7 @@ class User(db.Model):
 
 # Category Model
 class Category(db.Model):
-    id = db.Column(db.String(11), primary_key=True, default=get_uuid)
+    id = db.Column(Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     expenses = db.relationship('Expense', backref='category', lazy=True)
 
@@ -29,7 +29,7 @@ class Category(db.Model):
 
 # Expense Model
 class Expense(db.Model):
-    id = db.Column(db.String(11), primary_key=True, default=get_uuid)
+    id = db.Column(Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
