b614ebab5d5c83f0d4f4961a14743c9314a96c90
Glory Gweth <glorygweth6@gmail.com>
2023-10-08 18:23:31 +0300

    update post expense

--- models.py
+++ models.py
@@ -6,12 +6,12 @@ db = SQLAlchemy()
 # User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(60), nullable=False)
     expenses = db.relationship('Expense', backref='user', lazy=True)
 
-    def _repr_(self):
+    def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
 # Category Model
@@ -20,17 +20,17 @@ class Category(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     expenses = db.relationship('Expense', backref='category', lazy=True)
 
-    def _repr_(self):
+    def __repr__(self):
         return f"Category('{self.name}')"
 
 # Expense Model
 class Expense(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
-    def _repr_(self):
+    def __repr__(self):
         return f"Expense('{self.name}', '{self.amount}', '{self.date}')"
